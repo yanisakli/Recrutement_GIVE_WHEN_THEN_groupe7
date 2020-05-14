480beef88606b13e25022ef2890a95181a152a15
Yanis <yanisakli@yahoo.fr>
2020-05-14 14:52:12 +0200

    :wrench: Update Interview annulation

--- src/main/java/use_case/InterviewAnnulation.java
+++ src/main/java/use_case/InterviewAnnulation.java
@@ -2,6 +2,14 @@ package main.java.use_case;
 
 import main.java.model.interview.Interview;
 
-public class interviewAnnulation {
-    
+public class InterviewAnnulation {
+    private final Interview interview;
+
+    public InterviewAnnulation(Interview interview) {
+        this.interview = interview;
+    }
+
+    public void cancelInterview(){
+        interview.cancelInterview();
+    }
 }
