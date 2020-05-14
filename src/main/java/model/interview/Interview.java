80d4726783af4a3b2e6dba637b8a15ca2f61c39d
Yanis <yanisakli@yahoo.fr>
2020-05-14 14:10:51 +0200

    :fire: Edit interview

--- src/main/java/model/interview/Interview.java
+++ src/main/java/model/interview/Interview.java
@@ -8,16 +8,68 @@ import java.util.UUID;
 
 public class Interview {
     private final UUID interviewUuid;
-    private final Candidat interviewCandidat;
-    private final Recruiter interviewRecruiter;
-    private final String interviewDate;
-    private final Room interviewRoom;
+    private Candidat interviewCandidat;
+    private Recruiter interviewRecruiter;
+    private String interviewDate;
+    private Room interviewRoom;
+    private Slot slot;
+    private Status status;
     
-    public Interview(Candidat candidat, Recruiter recruiter, String date, Room room) {
+    public Interview(Candidat candidat, Recruiter recruiter, String date, Room room, Slot slot) {
         this.interviewUuid = UUID.randomUUID();
         this.interviewCandidat = candidat;
         this.interviewRecruiter = recruiter;
         this.interviewDate = date;
         this.interviewRoom = room;
+        this.slot = slot;
+        setStatus(Status.CONFIRME);
+    }
+
+    public Candidat getInterviewCandidat() {
+        return interviewCandidat;
+    }
+
+    public void setInterviewCandidat(Candidat interviewCandidat) {
+        this.interviewCandidat = interviewCandidat;
+    }
+
+    public Recruiter getInterviewRecruiter() {
+        return interviewRecruiter;
+    }
+
+    public void setInterviewRecruiter(Recruiter interviewRecruiter) {
+        this.interviewRecruiter = interviewRecruiter;
+    }
+
+    public String getInterviewDate() {
+        return interviewDate;
+    }
+
+    public void setInterviewDate(String interviewDate) {
+        this.interviewDate = interviewDate;
+    }
+
+    public Room getInterviewRoom() {
+        return interviewRoom;
+    }
+
+    public void setInterviewRoom(Room interviewRoom) {
+        this.interviewRoom = interviewRoom;
+    }
+
+    public Slot getSlot() {
+        return slot;
+    }
+
+    public void setSlot(Slot slot) {
+        this.slot = slot;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
---
+++ src/main/java/model/interview/Interviews.java
@@ -0,0 +1,4 @@
+package main.java.model.interview;
+
+public interface Interviews {
+}
